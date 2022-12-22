974c32d3cdb67d4891140f704af0bfc6c82df3e3
Anagha Radhakrishnan Moosad <anaghamoosad@gmail.com>
2022-12-22 20:32:57 +0300

    changing the default color of button shadow

--- static/app.js
+++ static/app.js
@@ -1,36 +1,31 @@
 $(document).ready(function() {
-       
-    var socket = io();   
     
-    // Test response received from server is handled here
-    socket.on('test_response', function(msg) {  
-
-        var element2 = document.getElementById("newprog");  
-        var element3 = document.getElementById("proglbl")  
-
-
-        if (msg.data==100) {
-            element2.style.width=100;
-            document.getElementById("video").onclick = function() {
-                document.getElementById("progressbars").style.visibility= "hidden";
-            }
-            
-        } 
-        if (msg.data==100) {
-            element3.style.width=100;
-        } 
-        else {
-                    
-        element2.style.width = msg.data + '%'; 
-   
-        element3.textContent = msg.data + '%'; 
-    }
-    })
-
+    //show progressbar on download_video button click
     document.getElementById("video").onclick = function() {
         document.getElementById("progressbars").style.visibility= "visible";
-    }
-     
+    }  
+    //show progressbar on download_audio button click
+    document.getElementById("audio").onclick = function() {
+        document.getElementById("progressbars").style.visibility= "visible";
+    }  
 
+    // get progressbar percentage using socket connection
+    var socket = io();
+    socket.on('progress_perct', function(msg) {  
+        var prog_bar = document.getElementById("newprog");  
+        var prog_lbl = document.getElementById("proglbl")  
+
+        if (msg.data==100) {
+            prog_bar.style.width=100;
+            document.getElementById("video").onclick = function() {
+                document.getElementById("progressbars").style.visibility= "hidden";
+            }  
+            prog_lbl.style.width=100;          
+        }
+        else {                    
+            prog_bar.style.width = msg.data + '%';    
+            prog_lbl.textContent = msg.data + '%'; 
+    }
+    })
 });
  
