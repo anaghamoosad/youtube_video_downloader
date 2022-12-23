1940a134be4ee6bd4edd433b11568d0e3ca9b162
Anagha Radhakrishnan Moosad <anaghamoosad@gmail.com>
2022-12-23 20:42:21 +0300

    Latest working code with flash message

--- static/app.js
+++ static/app.js
@@ -1,31 +1,35 @@
 $(document).ready(function() {
-    
-    //show progressbar on download_video button click
-    document.getElementById("video").onclick = function() {
-        document.getElementById("progressbars").style.visibility= "visible";
-    }  
-    //show progressbar on download_audio button click
-    document.getElementById("audio").onclick = function() {
-        document.getElementById("progressbars").style.visibility= "visible";
-    }  
+ 
 
     // get progressbar percentage using socket connection
     var socket = io();
     socket.on('progress_perct', function(msg) {  
         var prog_bar = document.getElementById("newprog");  
-        var prog_lbl = document.getElementById("proglbl")  
+        var prog_lbl = document.getElementById("proglbl")                 
+        prog_bar.style.width = msg.data + '%';    
+        prog_lbl.textContent = msg.data + '%'; 
+        document.getElementById("progressbars").style.visibility= "visible";   
+ 
+    });
 
-        if (msg.data==100) {
-            prog_bar.style.width=100;
-            document.getElementById("video").onclick = function() {
-                document.getElementById("progressbars").style.visibility= "hidden";
-            }  
-            prog_lbl.style.width=100;          
-        }
-        else {                    
-            prog_bar.style.width = msg.data + '%';    
-            prog_lbl.textContent = msg.data + '%'; 
-    }
-    })
+   
+    //show progressbar on download_video button click
+    document.getElementById("video").onclick = function() {
+        document.getElementById("progressbars").style.visibility= "visible";       
+    }  
+    //show progressbar on download_audio button click
+    document.getElementById("audio").onclick = function() {
+        document.getElementById("progressbars").style.visibility= "visible";       
+    }  
+
+        //show progressbar on download_video button click
+        document.getElementById("typeText").onclick = function() {
+            document.getElementById("progressbars").style.visibility= "hidden"; 
+            const element = document.getElementById("flash-msg");
+    	    element.remove();
+        }  
+
 });
+
+ 
  
